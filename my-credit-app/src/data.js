af0a32a927aa61ce9ce92ce3bf1f51249ca5e150
Asmaa diani <dianiasmaa27328@gmail.com>
2026-04-23 23:38:32 +0100

    créer app react et commencer la creation d'objet

---
+++ my-credit-app/src/data.js
@@ -0,0 +1,6 @@
+export const creditOffers = [
+    {id : 1, provider: "BMCE", amount: 100000, rate: 3.5, duration: 10},
+    {id : 2, provider: "CIH", amount: 150000, rate: 2.9, duration: 15},
+    {id: 3, provider: "Attijari", amount: 120000, rate: 4.2,duration:12},
+    {id: 4, provider: "Populaire", amount:200000, rate: 3.1, duration: 20}
+];
