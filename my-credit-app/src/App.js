33cd7e8dc24d9d57af9a6f2f092c285743d08f24
Asmaa diani <dianiasmaa27328@gmail.com>
2026-04-24 18:58:41 +0100

    gérer les erreur et ajouter useState pour details

--- my-credit-app/src/App.js
+++ my-credit-app/src/App.js
@@ -1,20 +1,36 @@
-import './App.css';
+import { useState } from 'react';
 import { creditOffers } from "./data.js";
-import OfferCard from "./components/OfferCard";
+import OfferCard from "./components/OfferCard.js"
 
 function App() {
+  const [selectedOffer, setSelectedOffer] = useState(null);
   const minRate = Math.min(...creditOffers.map(o => o.rate));
 
   return (
-    <div className="App">
+    <div className="App"> {/* <--- Darori had l-Container l-kbir */}
       <h1>Comparateur de Crédit</h1>
-      <div className="offers-list" style={{ display: 'flex', flexWrap: 'wrap' }}>
+      
+      <div className="offers-list" style={{ display: 'flex', justifyContent: 'center' }}>
         {creditOffers.map((offre) => (
-          <OfferCard key={offre.id} offre={offre} isRecommended={offre.rate === minRate} />
-          // devienne isRecommended sauf s'il est la petite valeur 
+          <OfferCard 
+            key={offre.id} 
+            offre={offre} 
+            isRecommended={offre.rate === minRate} // vérifier si c'est la valeur minimal pour devienne true 
+            onSelect={() => setSelectedOffer(offre)} // Siftna l-clic event k-prop
+          />
         ))}
       </div>
-    </div>
+
+      {selectedOffer && (
+        <div className="details-section" style={{ marginTop: '20px', padding: '20px', backgroundColor: '#f9f9f9', borderRadius: '10px' }}>
+          <h2>Détails de l'offre: {selectedOffer.provider}</h2>
+          <p>Durée: {selectedOffer.duration} ans</p>
+          <p>Mensualité: {(selectedOffer.amount / (selectedOffer.duration * 12)).toFixed(2)} DH/mois</p>
+          <p>Coût Total: {selectedOffer.amount} DH</p>
+        </div>
+      )}
+
+    </div> 
   );
 }
 
--- my-credit-app/src/components/OfferCard.js
+++ my-credit-app/src/components/OfferCard.js
@@ -1,12 +1,12 @@
-function OfferCard( {offre, isRecommended} ){
-    return (
-        <div className="offer-card" style={{ border: '1px solid #ccc', margin: '10px', padding: '15px', borderRadius: '8px' }}>
-            {isRecommended && <span className="badge">Recommended</span>}
-            <h3>{offre.provider}</h3>
-            <p>Montant: {offre.amount} DH</p>
-            <p>Taux: {offre.rate}%</p>
-            <button>Voir Détails</button>
-        </div>
-    );
+function OfferCard({ offre, isRecommended, onSelect }) {
+  return (
+    <div className="offer-card" onClick={onSelect} style={{ cursor: 'pointer'}}>
+      {isRecommended && <span className="badge">Recommended</span>}
+      <h3>{offre.provider}</h3>
+      <p>Montant: {offre.amount} DH</p>
+      <p>Taux: {offre.rate}%</p>
+      <button>Voir Détails</button>
+    </div>
+  );
 }
 export default OfferCard;
