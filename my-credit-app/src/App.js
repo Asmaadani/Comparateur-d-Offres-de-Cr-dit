6337ecb166b12d96f6ed81384bc19d913bb76a76
Asmaa diani <dianiasmaa27328@gmail.com>
2026-04-26 00:45:07 +0100

    travailler sur le design

--- my-credit-app/src/App.js
+++ my-credit-app/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import './App.css';
 import { creditOffers } from "./data.js";
 import OfferCard from "./components/OfferCard.js"
 import CompoundInterest from "./components/CompoundInterest";
